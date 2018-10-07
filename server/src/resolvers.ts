929b21525e2969facb6990ce2a82f6e9dafe796f
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-07 15:10:53 -0700

    initial commit

---
+++ server/src/resolvers.ts
@@ -0,0 +1,20 @@
+import { IResolvers } from "graphql-tools"; 
+import * as bcrypt from 'bcryptjs'; 
+import { User } from "./entity/User";
+
+export  const resolvers: IResolvers = {
+  Query: {
+    hello: () => "hi"
+  },
+  Mutation: {
+    register: async (_, {email, password}) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await User.create({
+        email,
+        password: hashedPassword
+      }).save();
+
+      return true;
+    }
+  }
+};
