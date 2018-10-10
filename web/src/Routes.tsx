759a734ff3612c096ec7b2546822b68115674e45
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-10 14:51:40 -0700

    getting token back from stripe

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import {LoginView} from "./modules/user/LoginView"
 import {RegisterView} from "./modules/user/RegisterView"
 import {MeView} from "./modules/user/MeView"
+import SubscribeUser from "./modules/account/SubscribeUser";
 
 export class Routes extends React.PureComponent {
   render() {
@@ -11,6 +12,7 @@ export class Routes extends React.PureComponent {
       <Route path="/login" component={LoginView} />
       <Route path="/register" component={RegisterView} />
       <Route path="/me" component={MeView} />
+      <Route path="/subscription" component={SubscribeUser} />
     </Switch>
     </BrowserRouter>);
   }
--- web/src/modules/user/LoginView.tsx
+++ web/src/modules/user/LoginView.tsx
@@ -1,9 +1,74 @@
 import * as React from 'react'
+import { Mutation } from "react-apollo"
+import { gql } from "apollo-boost"
+import { RouteComponentProps } from "react-router-dom"
 
-export class LoginView extends React.PureComponent {
-  render(){
-    return (<div>
-      login view
-    </div>);
+import { LoginMutationVariables, LoginMutation } from "../../schemaTypes"
+
+const loginMutation = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      email
+    }
+  }
+`
+
+export class LoginView extends React.PureComponent<RouteComponentProps<{}>> {
+  
+  state = {
+    email: '',
+    password: ''
+  }
+  
+  handleChange = (e: any) => {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const {password, email} = this.state;
+    return (
+      <Mutation<LoginMutation, LoginMutationVariables> mutation={loginMutation}
+      >
+        {mutate => (
+          <div style={{display: 'flex', flexDirection: "column", alignItems: "center", justifyContent:"center", marginTop:"2rem"}}>
+          <div>
+            <input
+            type="text"
+            name="email" 
+            placeholder="email"
+            value={email} 
+            onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input 
+            type="password"
+            name="password" 
+            placeholder="password"
+            value={password} 
+            onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button
+            onClick={async () => {
+              const response = await mutate ({
+                variables: this.state
+              });
+              console.log(response);
+              this.props.history.push("/me");
+            }}
+            >
+            login
+            </button>
+          </div>
+    </div>
+    )}
+    </Mutation>
+    );
   }
 }
